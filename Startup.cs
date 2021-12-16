f795674862cc6e4eec6776cb2ad2d53c7115c8c0
[Bhavesh Injamara] <[bhaveshdev.net@gmail.com]>
2021-12-15 20:47:05 -0600

    Sync 12/15/2021

--- Startup.cs
+++ Startup.cs
@@ -44,6 +44,8 @@ namespace azure
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "azure v1"));
             }
 
+            app.UseDefaultFiles();
+            
             app.UseStaticFiles();
 
             app.UseRouting();
